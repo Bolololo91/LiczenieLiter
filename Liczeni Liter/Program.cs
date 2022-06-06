f0f2fa7d212fc4a8fb5040870b5ba9b247588361
Bolololo91 <karolczarnecki5@gmail.com>
2022-06-06 12:32:22 +0200

    Dodaj pliki projektów.

---
+++ Liczeni Liter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liczeni_Liter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            char letter = Console.ReadKey().KeyChar;
+            char[] textchar = text.ToArray();
+            int counter = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (textchar[i] == letter)
+                {
+                    counter++;
+                }
+               
+            }
+            Console.Clear();
+            Console.WriteLine("Szukanych liter Jest " + counter);
+            Console.ReadLine();
+
+
+        }
+    }
+}
